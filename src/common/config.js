4f2fb8631496ed5ba40e375e18d859845f24af76
wth844311882 <1353962205@qq.com>
2020-04-26 12:00:04 +0800

    :wrench:update config

--- src/common/config.js
+++ src/common/config.js
@@ -1,12 +1,12 @@
 const firstRoute = ''
 const shareLink_prefix = "http://localhost/todo/#/"
-const domain = "http://localhost:3000/"
-const serverDomain = "http://localhost:3001/"
-const wsDomain = "ws://localhost:3002/"
+const domain = `http://localhost:3000/`
+const serverDomain = `http://localhost:3001/`
+const wsDomain = `ws://localhost:3002/`
 
 const jwt = {
     exp: 60 * 60,
-    secretKey: 'your_secretKey',
+    secretKey: 'WTH30haha',
 }
 
 const version = 'v1.0.1'
